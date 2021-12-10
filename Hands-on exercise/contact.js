12101ab5112bd2bcf9732d9e1b1206f96bba3699
unknown <anik.bmsit@gmail.com>
2021-12-10 08:42:01 +0530

    Adding all my pages

---
+++ Hands-on exercise/contact.js
@@ -0,0 +1,11 @@
+function CallContact() {
+   var retVal = confirm("Do u want to continue");
+   if(retVal == true){
+      alert("Call :- +91- 99883377465");
+   }
+   else {
+      alert("Y what happened");
+   }
+}
+
+
